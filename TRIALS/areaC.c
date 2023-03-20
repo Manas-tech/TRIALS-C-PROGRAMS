b7b5d74ba72b6ec5f475885e03632dab064a1b2b
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-03-20 21:58:05 +0530

    Update areaC.c

--- TRIALS/areaC.c
+++ TRIALS/areaC.c
@@ -5,7 +5,7 @@ int main() {
     int area ;
     printf ("enter your radius\n");
     scanf ("%d",&radius);
-    printf ("your radius is %d\n",radius);
+    printf ("your radius is  %d\n",radius);
     pie=3;
     printf ("the area of circle is%d",3*radius*radius);
 return 0;
